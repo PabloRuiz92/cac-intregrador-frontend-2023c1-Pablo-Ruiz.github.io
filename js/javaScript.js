92996156c92214953d130815c9dbfad3d7abb748
PabloRuiz <pabloruiz1992@outlook.com.ar>
2023-05-31 10:13:23 -0300

    agregada la validacion y advertencia. finalizado

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,9 +1,24 @@
 let clickSubmit=document.getElementById("btnSubmit");
+let clickReset=document.getElementById("btnReset");
 
 clickSubmit.addEventListener("click",()=>{
     let ticket = 200;
     let cantidadTickets = document.getElementById("cantidadTickets").value;
-    let porcentaje = document.getElementById("categoria").value;
-    let ticketConDescuento = ticket - ((porcentaje * ticket)/100);
-    document.getElementById("valorFinal").innerText = ticketConDescuento*cantidadTickets;
+    if (!isNaN(cantidadTickets) && cantidadTickets >= 1) {
+        let porcentaje = document.getElementById("categoria").value;
+        let ticketConDescuento = descuentoPorPorcentaje(ticket,porcentaje);
+        document.getElementById("valorFinal").innerText = ticketConDescuento*cantidadTickets;
+    } else {
+        document.getElementById("advertencia").style.visibility="visible";
+    }
 });
+
+clickReset.addEventListener("click",()=>{
+    document.getElementById("valorFinal").innerText = "";
+    document.getElementById("advertencia").style.visibility="hidden";
+})
+
+function descuentoPorPorcentaje(precioOriginal,porcentaje){
+    let precioConDescuento = precioOriginal - ((porcentaje * precioOriginal)/100);
+    return precioConDescuento;
+}
